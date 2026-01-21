e01ccc87e7c7e46832283294d6af6b7c2a7f0c74
Gabriel <br.gabrielyra@gmail.com>
2026-01-21 11:59:18 -0300

    testes de funcao

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-const animaisSection = document.getElementById('animais')
-console.log(animais)
+const animaisSection = document.getElementById('animais');
+const linksInternos = document.querySelectorAll('[href^="#"]');
+console.log(linksInternos);
+
+
+console.log(animais);
+
+const primeiroUl = document.querySelector('ul');
+const navItem = primeiroUl.querySelector('li');
+
+console.log(navItem);  
+
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos[-- paragrafos.length]);
