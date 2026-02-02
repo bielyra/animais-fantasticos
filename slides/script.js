c795099cd33876f26451850fce87216e2b0ffb11
Gabriel <br.gabrielyra@gmail.com>
2026-02-02 11:36:50 -0300

    novas animações de scroll e início do setor de javascript intermediário

--- slides/script.js
+++ slides/script.js
@@ -1,39 +1,91 @@
-function initTabNav(){
-    
+function initTabNav() {
+
     const tabMenu = document.querySelectorAll('.js-tabmenu li');
     const tabContent = document.querySelectorAll('.js-tabcontent section');
     tabContent[0].classList.add('ativo');
-    
+
     // Verificar se existe elemento em tabContent e tabMenu
-    if(tabContent.length && tabMenu.length) {
-      tabContent[0].classList.add('ativo');
-      
-      function activeTab(index) {
-          tabContent.forEach((section) => {
-              section.classList.remove('ativo');
-          });
-          tabContent[index].classList.add('ativo');
-      }
-      
-      tabMenu.forEach((itemMenu, index) => {
-          itemMenu.addEventListener('click', () => {
-              activeTab(index);
-          });
-      });
-    
+    if (tabContent.length && tabMenu.length) {
+        tabContent[0].classList.add('ativo');
+
+        function activeTab(index) {
+            tabContent.forEach((section) => {
+                section.classList.remove('ativo');
+            });
+            tabContent[index].classList.add('ativo');
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            });
+        });
+
     }
 
 }
 initTabNav();
 
-const accordionList = document.querySelectorAll('.js-accordion dt');
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const ativoClass = 'ativo';
 
-function activeAccordion() {
-    event.currentTarget.classList.toggle('ativo');
-}
 
-accordionList.forEach((item) => {
-    item.addEventListener('click', activeAccordion); {
+    if (accordionList.length) {
+        accordionList[0].classList.add(ativoClass);
+        accordionList[0].nextElementSibling.classList.add(ativoClass);
 
+        function activeAccordion() {
+            this.classList.toggle(ativoClass);
+            this.nextElementSibling.classList.toggle(ativoClass);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click', activeAccordion);
+        });
     }
-});
+}
+initAccordion();
+
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        section.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+        });
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click', scrollToSection);
+    });
+}
+initScrollSuave();
+
+const sections = document.querySelectorAll('.js-scroll');
+
+// Transforme o objeto abaixo em uma Constructor Function
+/*const pessoa = {
+  nome: 'Nome pessoa',
+  idade: 0,
+  andar() {
+    console.log(this.nome + ' andou');
+  }
+}
+*/
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
+
+
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
+
