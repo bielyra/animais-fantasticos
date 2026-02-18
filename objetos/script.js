8d89f57bbc1d63a2ec32704aa28d4458b36dbba5
Gabriel <br.gabrielyra@gmail.com>
2026-02-18 16:28:45 -0300

    Exercicios iteração

--- objetos/script.js
+++ objetos/script.js
@@ -327,7 +327,7 @@ console.log(array.sort((a, b) => a - b))
 console.log(array.sort((a, b) => b - a))
 ////////////////////////////////////////////////////
 */
-const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão']; 
+/* const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão']; 
 
 // Remova o primeiro valor de comidas e coloque em uma variável
 const primeiroValor = comidas.shift();
@@ -387,6 +387,78 @@ const antigoArrayCarros = carros.slice();
 console.log(carros.pop());
 console.log(carros);
 console.log(antigoArrayCarros);
+ */
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+
+const cursos = document.querySelectorAll('.curso')
+const arrayCursos = Array.from(cursos)
+console.log(arrayCursos);
+
+const objetosCurso = arrayCursos.map((curso) =>{
+  const titulo = curso.querySelector('h1').innerText;
+  const descricao = curso.querySelector('p').innerText;
+  const horas = curso.querySelector('.horas').innerText;
+  const aulas = curso.querySelector('.aulas').innerText;
 
 
+  return{
+    titulo,
+    descricao,
+  }
+  
+})
 
+console.log(objetosCurso);
+
+
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+const maioresQue100 = numeros.filter(n => n > 100);
+console.log(maioresQue100);
+
+
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+const possuiBaixo = instrumentos.some((item) =>{
+  return item === 'Baixo';
+})
+
+console.log(possuiBaixo);
+
+
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
+
+const valorTotal = compras.reduce((acumulador, item) =>{
+const precoLimpo = +item.preco.replace('R$', '').replace(',','.')
+
+return acumulador + precoLimpo;
+
+
+}, 0)
+console.log(valorTotal);
