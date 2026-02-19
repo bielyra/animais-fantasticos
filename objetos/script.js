4ae55be94f00ba63317df5141054681ca85a472b
Gabriel <br.gabrielyra@gmail.com>
2026-02-19 12:08:23 -0300

    Functions e só falta object agora

--- objetos/script.js
+++ objetos/script.js
@@ -391,7 +391,7 @@ console.log(antigoArrayCarros);
 // Selecione cada curso e retorne uma array
 // com objetos contendo o título, descricao,
 // aulas e horas de cada curso
-
+/* 
 const cursos = document.querySelectorAll('.curso')
 const arrayCursos = Array.from(cursos)
 console.log(arrayCursos);
@@ -451,6 +451,10 @@ const compras = [
   {
     item: 'Quejo',
     preco: 'R$ 10,60'
+  },
+  {
+    item: 'Arroz',
+    preco: 'R$ 9,34'
   }
 ]
 
@@ -461,4 +465,52 @@ return acumulador + precoLimpo;
 
 
 }, 0)
-console.log(valorTotal);
+console.log(+valorTotal.toFixed(2));
+
+ */
+ // 0408 Function 1
+
+ function areaQuadrado(lado) {
+  return lado * lado;
+}
+
+const perimetroQuadrado = new Function('lado', 'return lado * 4');
+
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+const paragrafos = document.querySelectorAll('p');
+
+const totalCaracteres = Array.prototype.reduce.call(paragrafos, (acumulador, item) => {
+  console.log(item.innerText.length);
+  return acumulador + item.innerText.length;
+  
+},0);
+console.log('------------');
+
+console.log(totalCaracteres);
+
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+
+function criarElemento(tag,classe,conteudo){
+  const elemento = document.createElement(tag);
+  classe ? elemento.classList.add(classe) : null;
+  conteudo ? elemento.innerHTML = conteudo : null;
+
+  return elemento;
+}
+console.log(criarElemento('li', 'Jorge','AlelalaelAue'));
+
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
+
+const h1Titulo = criarElemento.bind(null, 'h1', 'titulo')
+
+console.log(h1Titulo('Cursos de JavaScript'));
+console.log(h1Titulo('Cursos de HTML'));
+
+
+
