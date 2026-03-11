054496832a1c1361cb2030abaa86ff26d586385d
Gabriel <br.gabrielyra@gmail.com>
2026-03-11 10:04:49 -0300

    nova pasta, modal e novas funcoes

---
+++ efeitos-no-dom/modal/js/script.js
@@ -0,0 +1,8 @@
+import initTabNav from "./modules/tab-nav.js";
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAccordion from "./modules/accordion.js";
+import initAnimacaoScroll from "./modules/animacao-scroll.js";
+initScrollSuave();
+initTabNav();
+initAccordion();
+initAnimacaoScroll();
