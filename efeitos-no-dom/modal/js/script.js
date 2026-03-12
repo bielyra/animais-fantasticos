8b70cceca43e27db9d274627b7647733b2884cd1
Gabriel <br.gabrielyra@gmail.com>
2026-03-12 10:23:16 -0300

    criação do modal.js e alterações

--- efeitos-no-dom/modal/js/script.js
+++ efeitos-no-dom/modal/js/script.js
@@ -2,7 +2,10 @@ import initTabNav from "./modules/tab-nav.js";
 import initScrollSuave from "./modules/scroll-suave.js";
 import initAccordion from "./modules/accordion.js";
 import initAnimacaoScroll from "./modules/animacao-scroll.js";
+import initModal from "./modules/modal.js";
+
 initScrollSuave();
 initTabNav();
 initAccordion();
 initAnimacaoScroll();
+initModal();
